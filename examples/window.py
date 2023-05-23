40a343ab66e9648d51140fe15196af9051bb9b7d
Kacper Klassa <kacperklassa@gmail.com>
2023-05-24 00:53:27 +0200

    initial commit

---
+++ examples/window.py
@@ -0,0 +1,24 @@
+import glfw
+
+def main():
+    # Initialize the library
+    if not glfw.init():
+        return
+    # Create a windowed mode window and its OpenGL context
+    window = glfw.create_window(640, 480, "Hello World", None, None)
+    if not window:
+        glfw.terminate()
+        return
+
+    # Loop until the user closes the window
+    while not glfw.window_should_close(window):
+        # Swap front and back buffers
+        glfw.swap_buffers(window)
+
+        # Poll for and process events
+        glfw.poll_events()
+
+    glfw.terminate()
+
+if __name__ == "__main__":
+    main()
